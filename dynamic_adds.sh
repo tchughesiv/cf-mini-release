2d54f0fe7ba8358ed3b07d9ca3716bb13f09dfe0
tchughesiv <tchughesiv@gmail.com>
2015-05-17 09:19:10 -0500

    update

--- dynamic_adds.sh
+++ dynamic_adds.sh
@@ -5,3 +5,4 @@ NISE_IP_ADDRESS=${NISE_IP_ADDRESS:-`ip addr | grep 'inet .*global' | cut -f 6 -d
 sed -i 's/buildpack_java/buildpack_java_offline/g' /root/cf_nise_installer/manifests/template.yml
 sed -i 's/^    hm9000_noop: false/    hm9000_noop: false\n    default_app_memory: 512\n    default_app_disk_in_mb: 512/g' /root/cf_nise_installer/manifests/template.yml
 echo 'sed -i "s/${NISE_IP_ADDRESS}/0.0.0.0/g" manifests/deploy.yml' >> /root/cf_nise_installer/scripts/generate_deploy_manifest.sh
+echo ${NISE_IP_ADDRESS} > /root/PRIOR_NISE_IP_ADDRESS
