d2c3dba87242322b29d3672dee7e14098ee0b683
tchughesiv <tchughesiv@gmail.com>
2015-05-17 14:22:00 -0500

    update

--- dynamic_adds.sh
+++ dynamic_adds.sh
@@ -3,7 +3,7 @@
 # NISE_IP_ADDRESS=${NISE_IP_ADDRESS:-`ip addr | grep 'inet .*global' | cut -f 6 -d ' ' | cut -f1 -d '/' | head -n 1`}
 sed -i 's/buildpack_java/buildpack_java_offline/g' /root/cf_nise_installer/manifests/template.yml
 sed -i 's/^    hm9000_noop: false/    hm9000_noop: false\n    default_app_memory: 512\n    default_app_disk_in_mb: 512/g' /root/cf_nise_installer/manifests/template.yml
-# sed -i 's/#{ip_address}\/16/0.0.0.0\/32/g' /root/cf_nise_installer/nise_bosh/lib/nise_bosh/builder.rb
+sed -i 's/#{ip_address}\/16/0.0.0.0\/0/g' /root/cf_nise_installer/nise_bosh/lib/nise_bosh/builder.rb
 # sed -i 's/#{ip_address}/0.0.0.0/g' /root/cf_nise_installer/nise_bosh/lib/nise_bosh/builder.rb
 # sed -i 's/= ip_address/= 0.0.0.0/g' /root/cf_nise_installer/nise_bosh/lib/nise_bosh/builder.rb
 # echo 'sed -i "s/${NISE_IP_ADDRESS}/0.0.0.0/g" manifests/deploy.yml' >> /root/cf_nise_installer/scripts/generate_deploy_manifest.sh
