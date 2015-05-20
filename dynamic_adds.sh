49d0d8aa202a8552932e5845cf6590ede8593999
tchughesiv <tchughesiv@gmail.com>
2015-05-19 22:11:10 -0500

    update

--- dynamic_adds.sh
+++ dynamic_adds.sh
@@ -3,6 +3,7 @@ NISE_IP_ADDRESS=${NISE_IP_ADDRESS:-`ip addr | grep 'inet .*global' | cut -f 6 -d
 echo ${NISE_IP_ADDRESS} > /root/PRIOR_NISE_IP_ADDRESS
 sed -i 's/buildpack_java/buildpack_java_offline/g' /root/cf_nise_installer/manifests/template.yml
 sed -i 's/^    hm9000_noop: false/    hm9000_noop: false\n    default_app_memory: 512\n    default_app_disk_in_mb: 512/g' /root/cf_nise_installer/manifests/template.yml
+sed -i 's/disk_quota_enabled: true/disk_quota_enabled: false/g' /root/cf_nise_installer/manifests/template.yml
 sed -i '/install_ruby.sh/d' /root/cf_nise_installer/scripts/install.sh
 # sed -i '/install_cf_release.sh/d' /root/cf_nise_installer/scripts/install.sh
 sed -i '/apt-get update/d' /root/cf_nise_installer/scripts/install.sh
