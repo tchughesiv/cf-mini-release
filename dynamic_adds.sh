e2a94eaddf174485cef355e81795b6fb2c495de6
tchughesiv <tchughesiv@gmail.com>
2015-05-20 17:38:14 -0500

    update

--- dynamic_adds.sh
+++ dynamic_adds.sh
@@ -5,8 +5,12 @@ sed -i 's/buildpack_java/buildpack_java_offline/g' /root/cf_nise_installer/manif
 sed -i 's/^    hm9000_noop: false/    hm9000_noop: false\n    default_app_memory: 512\n    default_app_disk_in_mb: 512/g' /root/cf_nise_installer/manifests/template.yml
 sed -i 's/disk_quota_enabled: true/disk_quota_enabled: false/g' /root/cf_nise_installer/manifests/template.yml
 sed -i 's/debug2/info/g' /root/cf_nise_installer/manifests/template.yml
-sed -i 's/z1:/default:/g' /root/cf_nise_installer/manifests/template.yml
-sed -i '/z2:/d' /root/cf_nise_installer/manifests/template.yml
+
+#???? remove these zone changes ????
+# sed -i 's/z1:/default:/g' /root/cf_nise_installer/manifests/template.yml
+# sed -i '/z2:/d' /root/cf_nise_installer/manifests/template.yml
+############
+
 sed -i '/install_ruby.sh/d' /root/cf_nise_installer/scripts/install.sh
 # sed -i '/install_cf_release.sh/d' /root/cf_nise_installer/scripts/install.sh
 sed -i '/apt-get update/d' /root/cf_nise_installer/scripts/install.sh
