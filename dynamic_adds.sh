0cf10d4d898b3fc8bb4d305611b9dc12d3281f57
tchughesiv <tchughesiv@gmail.com>
2015-05-20 22:12:31 -0500

    update

--- dynamic_adds.sh
+++ dynamic_adds.sh
@@ -4,6 +4,8 @@ echo ${NISE_IP_ADDRESS} > /root/PRIOR_NISE_IP_ADDRESS
 sed -i 's/buildpack_java/buildpack_java_offline/g' /root/cf_nise_installer/manifests/template.yml
 sed -i 's/^    hm9000_noop: false/    hm9000_noop: false\n    default_app_memory: 512\n    default_app_disk_in_mb: 512/g' /root/cf_nise_installer/manifests/template.yml
 sed -i 's/disk_quota_enabled: true/disk_quota_enabled: false/g' /root/cf_nise_installer/manifests/template.yml
+zone: default
+sed -i 's/zone: default/zone: z1/g' /root/cf_nise_installer/manifests/template.yml
 sed -i 's/debug2/info/g' /root/cf_nise_installer/manifests/template.yml
 
 #???? remove these zone changes ????
@@ -12,7 +14,6 @@ sed -i 's/debug2/info/g' /root/cf_nise_installer/manifests/template.yml
 ############
 
 sed -i '/install_ruby.sh/d' /root/cf_nise_installer/scripts/install.sh
-# sed -i '/install_cf_release.sh/d' /root/cf_nise_installer/scripts/install.sh
 sed -i '/apt-get update/d' /root/cf_nise_installer/scripts/install.sh
 sed -i '/apt-get update/d' /root/cf_nise_installer/scripts/install_environemnt.sh
 sed -i 's/.\/scripts\/clone_cf_release.sh/.\/scripts\/clone_cf_release.sh\n\/root\/dynamic_adds_2.sh/g' /root/cf_nise_installer/scripts/install.sh
