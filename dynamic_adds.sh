05d45119fb954fee79708abd8036aa55cb34851b
tchughesiv <tchughesiv@gmail.com>
2015-06-02 16:30:48 -0500

    update

--- dynamic_adds.sh
+++ dynamic_adds.sh
@@ -7,7 +7,7 @@ sed -i 's/disk_quota_enabled: true/disk_quota_enabled: false/g' /root/cf_nise_in
 sed -i 's/zone: default/zone: z1/g' /root/cf_nise_installer/manifests/template.yml
 sed -i '/- admin/ s/$/,uaa.admin,password.write/' /root/cf_nise_installer/manifests/template.yml
 sed -i 's/debug2/info/g' /root/cf_nise_installer/manifests/template.yml
-sed -i '/cc: \&cc/a \    droplets:\n      droplet_directory_key: c1oudc0w\n    buildpacks:\n      buildpack_directory_key: droplet_admin' /root/cf_nise_installer/manifests/template.yml
+sed -i '/cc: \&cc/a \    droplets:\n      droplet_directory_key: c1oudc0w\n    buildpacks:\n      buildpack_directory_key: c1oudc0w' /root/cf_nise_installer/manifests/template.yml
 
 #???? remove these zone changes ????
 # sed -i 's/z1:/default:/g' /root/cf_nise_installer/manifests/template.yml
