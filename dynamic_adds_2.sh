2a349500008f321174d64eaea3dce9cd806a1898
tchughesiv <tchughesiv@gmail.com>
2015-09-05 11:41:58 -0500

    update for trusty

--- dynamic_adds_2.sh
+++ dynamic_adds_2.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 sed -i '/apt-get update/d' /root/cf_nise_installer/nise_bosh/bin/init
-sed -i 's/peer-heartbeat-timeout/peer-heartbeat-interval/g' /root/cf_nise_installer/cf-release/jobs/etcd/templates/etcd_ctl.erb
+sed -i '/exit 1/d' /root/cf_nise_installer/nise_bosh/bin/init
+# sed -i 's/peer-heartbeat-timeout/peer-heartbeat-interval/g' /root/cf_nise_installer/cf-release/jobs/etcd/templates/etcd_ctl.erb
