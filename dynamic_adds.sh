eb5d8219c1d2ee0b7d0ddf317f6cf6d00ba35178
tchughesiv <tchughesiv@gmail.com>
2015-09-05 18:15:58 -0500

    update buildpacks

--- dynamic_adds.sh
+++ dynamic_adds.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 NISE_IP_ADDRESS=${NISE_IP_ADDRESS:-`ip addr | grep 'inet .*global' | cut -f 6 -d ' ' | cut -f1 -d '/' | head -n 1`}
 echo ${NISE_IP_ADDRESS} > /root/PRIOR_NISE_IP_ADDRESS
-sed -i 's/buildpack_java/buildpack_java_offline/g' /root/cf_nise_installer/manifests/template.yml
+sed -i 's/buildpack_java/buildpack_java_offline\n      - name: staticfile_buildpack\n        package: buildpack_staticfile\n      - name: binary_buildpack\n        package: buildpack_binary/g' /root/cf_nise_installer/manifests/template.yml
 sed -i 's/^    hm9000_noop: false/    hm9000_noop: false\n    default_app_memory: 512\n    default_app_disk_in_mb: 512/g' /root/cf_nise_installer/manifests/template.yml
 sed -i 's/disk_quota_enabled: true/disk_quota_enabled: false/g' /root/cf_nise_installer/manifests/template.yml
 sed -i 's/zone: default/zone: z1/g' /root/cf_nise_installer/manifests/template.yml
