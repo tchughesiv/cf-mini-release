0ae0e8b0dcd001e73603dd72fc7e8c3c1518db01
tchughesiv <tchughesiv@gmail.com>
2015-09-05 15:17:46 -0500

    consul removal

--- dynamic_adds_2.sh
+++ dynamic_adds_2.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 sed -i '/apt-get update/d' /root/cf_nise_installer/nise_bosh/bin/init
 sed -i '/exit 1/d' /root/cf_nise_installer/nise_bosh/bin/init
+sed -i '/consul_agent/d' /root/cf_nise_installer/cf-release/jobs/haproxy/monit
 # sed -i 's/peer-heartbeat-timeout/peer-heartbeat-interval/g' /root/cf_nise_installer/cf-release/jobs/etcd/templates/etcd_ctl.erb
