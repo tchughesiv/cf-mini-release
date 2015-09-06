2175bb2e7bdb8893a26f050665b820ca2927eb01
tchughesiv <tchughesiv@gmail.com>
2015-09-06 14:02:08 -0500

    permissions fixes

--- dynamic_adds.sh
+++ dynamic_adds.sh
@@ -5,15 +5,10 @@ sed -i 's/buildpack_java/buildpack_java_offline\n      - name: staticfile_buildp
 sed -i 's/^    hm9000_noop: false/    hm9000_noop: false\n    default_app_memory: 512\n    default_app_disk_in_mb: 512/g' /root/cf_nise_installer/manifests/template.yml
 sed -i 's/disk_quota_enabled: true/disk_quota_enabled: false/g' /root/cf_nise_installer/manifests/template.yml
 sed -i 's/zone: default/zone: z1/g' /root/cf_nise_installer/manifests/template.yml
-sed -i '/- admin/ s/$/,uaa.admin,password.write/' /root/cf_nise_installer/manifests/template.yml
+# sed -i '/- admin/ s/$/,uaa.admin,password.write/' /root/cf_nise_installer/manifests/template.yml
 sed -i 's/debug2/info/g' /root/cf_nise_installer/manifests/template.yml
 sed -i '/consul_agent/d' /root/cf_nise_installer/manifests/template.yml
 sed -i '/cc: \&cc/a \    droplets:\n      droplet_directory_key: cc-droplets\n    buildpacks:\n      buildpack_directory_key: cc-buildpacks\n    allow_app_ssh_access: false' /root/cf_nise_installer/manifests/template.yml
-
-#???? remove these zone changes ????
-# sed -i 's/z1:/default:/g' /root/cf_nise_installer/manifests/template.yml
-# sed -i '/z2:/d' /root/cf_nise_installer/manifests/template.yml
-############
 
 sed -i '/install_ruby.sh/d' /root/cf_nise_installer/scripts/install.sh
 sed -i '/apt-get update/d' /root/cf_nise_installer/scripts/install.sh
