4903a95535679575c864b00ac4eca0e0ff5a916c
tchughesiv <tchughesiv@gmail.com>
2016-06-13 15:41:15 -0500

    change consul dns port to 8600

--- dynamic_adds_2.sh
+++ dynamic_adds_2.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
 sed -i '/apt-get update/d' /root/cf_nise_installer/nise_bosh/bin/init
 sed -i '/exit 1/d' /root/cf_nise_installer/nise_bosh/bin/init
-# sed -i 's/peer-heartbeat-timeout/peer-heartbeat-interval/g' /root/cf_nise_installer/cf-release/jobs/etcd/templates/etcd_ctl.erb
+
+# change consul port to 8600
+sed -i 's/DNS: 53,/DNS: 8600,/g' /var/vcap/packages/confab/src/github.com/cloudfoundry-incubator/consul-release/src/confab/config/consul_config_definer.go
+sed -i 's/DNS: 53,/DNS: 8600,/g' /root/cf_nise_installer/cf-release/src/consul-release/src/confab/config/consul_config_definer.go
