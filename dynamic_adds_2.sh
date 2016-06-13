471d254fa9768c230d1c9a0324ab96b12ddda213
tchughesiv <tchughesiv@gmail.com>
2016-06-13 16:29:58 -0500

    consul fixes

--- dynamic_adds_2.sh
+++ dynamic_adds_2.sh
@@ -3,5 +3,6 @@ sed -i '/apt-get update/d' /root/cf_nise_installer/nise_bosh/bin/init
 sed -i '/exit 1/d' /root/cf_nise_installer/nise_bosh/bin/init
 
 # change consul port to 8600
-sed -i 's/DNS: 53,/DNS: 8600,/g' /var/vcap/packages/confab/src/github.com/cloudfoundry-incubator/consul-release/src/confab/config/consul_config_definer.go
+cd /root/cf_nise_installer/cf-release/src/consul-release/
+git submodule update --init
 sed -i 's/DNS: 53,/DNS: 8600,/g' /root/cf_nise_installer/cf-release/src/consul-release/src/confab/config/consul_config_definer.go
